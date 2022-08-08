703f16c095dbd724fb8741e6685c51a871ec2f59
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2022-08-07 21:20:58 -0400

    support local auth for testing

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,20 +1,51 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 )
 
-// TODO: allow auth bypass for local testing
+const HeaderAuthorization = "Authorization"
+
+type LocalAuthMiddleware struct {
+	AllowedToken string
+}
+
+func (m *LocalAuthMiddleware) LocalAuthVerify(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Read the API token from the header, and then verify that the
+		// token matches the allowed local token
+		if !m.compareToLocalToken(r) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (m *LocalAuthMiddleware) compareToLocalToken(r *http.Request) bool {
+	return m.AllowedToken == readAuthToken(r)
+}
+
 func AuthVerifyMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Read the API token from the header, and then verify the requestor
-		handleAuth()
+		if !validateToken(readAuthToken(r)) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
 }
 
-func handleAuth() {
-	log.Println("handle auth here")
+func readAuthToken(r *http.Request) string {
+	token, ok := r.Header[HeaderAuthorization]
+	if !ok {
+		return ""
+	}
+	return token[0]
+}
+
+func validateToken(token string) bool {
+	return true // TODO: finish this when hasAuth is done
 }
